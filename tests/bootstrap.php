3a74b8553e917d353334d91fd6314119423540d9
mvjdev <hei.maria.625@gmail.com>
2025-07-14 14:44:48 +0300

    fix: env online

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,10 +2,10 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 if (!isset($_SERVER['APP_ENV']) && class_exists(Dotenv::class)) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
 
 if ($_SERVER['APP_DEBUG'] ?? false) {
