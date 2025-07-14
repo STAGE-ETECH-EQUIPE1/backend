3867a3d1f3e41648466b929d7532d8a618f2ffc7
mvjdev <hei.maria.625@gmail.com>
2025-07-14 15:02:10 +0300

    fix: access to url backend

--- public/index.php
+++ public/index.php
@@ -2,16 +2,11 @@
 
 use App\Kernel;
 
-// S'assurer que les variables d’environnement sont prises en compte
-if (!isset($_SERVER['APP_ENV'])) {
-    $_SERVER['APP_ENV'] = getenv('APP_ENV') ?: 'prod';
-}
-if (!isset($_SERVER['APP_DEBUG'])) {
-    $_SERVER['APP_DEBUG'] = (bool) getenv('APP_DEBUG');
-}
+$_SERVER['APP_ENV'] = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? getenv('APP_ENV') ?: 'dev';
+$_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? (bool) getenv('APP_DEBUG');
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 };
