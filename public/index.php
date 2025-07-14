f3563abb47e64d53196eaf8280b3f1ddb5480683
mvjdev <hei.maria.625@gmail.com>
2025-07-14 14:52:57 +0300

    fix: env online

--- public/index.php
+++ public/index.php
@@ -2,17 +2,16 @@
 
 use App\Kernel;
 
+// S'assurer que les variables d’environnement sont prises en compte
 if (!isset($_SERVER['APP_ENV'])) {
-    $_SERVER['APP_ENV'] = getenv('APP_ENV');
+    $_SERVER['APP_ENV'] = getenv('APP_ENV') ?: 'prod';
 }
 if (!isset($_SERVER['APP_DEBUG'])) {
     $_SERVER['APP_DEBUG'] = (bool) getenv('APP_DEBUG');
 }
 
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-    var_dump($_SERVER['APP_ENV'], $_SERVER['APP_DEBUG'], $_SERVER['APP_SECRET'], $_SERVER['DATABASE_URL']);
-    exit;
 };
