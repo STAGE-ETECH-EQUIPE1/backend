a9e45ccd8a1da02dec5ba7c86aee63ce24cd4d9b
Hasintsoa <hasinaramalanjaona@gmail.com>
2025-07-14 12:07:47 +0300

    Perform request for user registration

--- src/Services/User/UserService.php
+++ src/Services/User/UserService.php
@@ -2,4 +2,41 @@
 
 namespace App\Services\User;
 
-final class UserService {}
+use App\DTO\Request\UserRegistrationDTO;
+use App\DTO\UserDTO;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+final class UserService implements UserServiceInterface
+{
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    ) {}
+
+    public function convertUserRegistrationDtoToUser(UserRegistrationDTO $userDto): User
+    {
+        $user = new User();
+        $user->setEmail($userDto->email)
+            ->setUsername($userDto->username)
+            ->setFullName($userDto->fullName)
+            ->setPhone($userDto->phone)
+            ->setRoles(['ROLE_USER'])
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->setPassword($this->passwordHasher->hashPassword(
+                $user,
+                $userDto->password
+            ));
+        return $user;
+    }
+
+    public function convertToDto(User $user): UserDTO
+    {
+        return (new UserDTO())
+            ->setId((int) $user->getId())
+            ->setEmail((string) $user->getEmail())
+            ->setUsername((string) $user->getUsername())
+            ->setFullName((string) $user->getFullName())
+            ->setPhone((string) $user->getPhone())
+            ->setCreatedAt($user->getCreatedAt() ?? new \DateTimeImmutable());
+    }
+}
--- src/Services/User/UserServiceInterface.php
+++ src/Services/User/UserServiceInterface.php
@@ -2,4 +2,23 @@
 
 namespace App\Services\User;
 
-interface UserServiceInterface{}
+use App\DTO\Request\UserRegistrationDTO;
+use App\DTO\UserDTO;
+use App\Entity\User;
+
+interface UserServiceInterface
+{
+    /**
+     * Convert UserRegistrationDTO to User entity.
+     * @param UserRegistrationDTO $userDto
+     * @return User
+     */
+    public function convertUserRegistrationDtoToUser(UserRegistrationDTO $userDto): User;
+
+    /**
+     * Convert User entity to UserDTO.
+     * @param User $user
+     * @return UserDTO
+     */
+    public function convertToDto(User $user): UserDTO;
+}
