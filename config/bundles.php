0c519d32e83c521f2d7c794809f9705829d4d567
Hasintsoa <hasinaramalanjaona@gmail.com>
2025-07-17 10:34:41 +0300

    feat: add debug bundle and configuration for development environment

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
--- tests/object-manager.php
+++ tests/object-manager.php
@@ -12,4 +12,5 @@ if (!isset($_SERVER['APP_ENV'])) {
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
 
+// @phpstan-ignore method.notFound
 return $kernel->getContainer()->get('doctrine')->getManager();
