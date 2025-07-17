0db5626ea8145acd61797fdf48ebdb2a9af7532d
Hasintsoa <hasinaramalanjaona@gmail.com>
2025-07-17 14:55:49 +0300

    feat: implement Notification entity with repository and enum for notification types

---
+++ src/Entity/Notification.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\NotificationType;
+use App\Repository\NotificationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NotificationRepository::class)]
+#[ORM\Table(name: '`notifications`')]
+class Notification
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $readAt = null;
+
+    #[ORM\Column(nullable: true, enumType: NotificationType::class)]
+    private ?NotificationType $type = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getReadAt(): ?\DateTimeImmutable
+    {
+        return $this->readAt;
+    }
+
+    public function setReadAt(?\DateTimeImmutable $readAt): static
+    {
+        $this->readAt = $readAt;
+
+        return $this;
+    }
+
+    public function getType(): ?NotificationType
+    {
+        return $this->type;
+    }
+
+    public function setType(?NotificationType $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+}
