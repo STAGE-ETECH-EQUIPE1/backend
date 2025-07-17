485014dfdb301df5d9135e9d20930d91e02b4a67
Hasintsoa <hasinaramalanjaona@gmail.com>
2025-07-17 15:08:32 +0300

    feat: create Order and Subscription entities with repositories and status enum

---
+++ src/Entity/Subscription/Subscription.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity\Subscription;
+
+use App\Enum\SubscriptionStatus;
+use App\Repository\SubscriptionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SubscriptionRepository::class)]
+#[ORM\Table(name: '`subscriptions`')]
+class Subscription
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $reference = null;
+
+    #[ORM\Column(enumType: SubscriptionStatus::class)]
+    private ?SubscriptionStatus $status = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $startedAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $endedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): static
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getStatus(): ?SubscriptionStatus
+    {
+        return $this->status;
+    }
+
+    public function setStatus(SubscriptionStatus $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getStartedAt(): ?\DateTimeImmutable
+    {
+        return $this->startedAt;
+    }
+
+    public function setStartedAt(\DateTimeImmutable $startedAt): static
+    {
+        $this->startedAt = $startedAt;
+
+        return $this;
+    }
+
+    public function getEndedAt(): ?\DateTimeImmutable
+    {
+        return $this->endedAt;
+    }
+
+    public function setEndedAt(\DateTimeImmutable $endedAt): static
+    {
+        $this->endedAt = $endedAt;
+
+        return $this;
+    }
+}
---
+++ src/Enum/SubscriptionStatus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enum;
+
+enum SubscriptionStatus: string
+{
+    case ACTIVE = 'active';
+    case INACTIVE = 'inactive';
+    case EXPIRED = 'expired';
+    case PENDING = 'pending';
+    case CANCELED = 'canceled';
+
+}
