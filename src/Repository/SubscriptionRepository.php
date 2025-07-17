c9d042b6661fc01ab051014b61b293b3592071ee
Hasintsoa <hasinaramalanjaona@gmail.com>
2025-07-17 16:10:07 +0300

    fix: correct namespace import for Subscription entity in SubscriptionRepository

--- src/Repository/SubscriptionRepository.php
+++ src/Repository/SubscriptionRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\App\Entity\Subscription;
+use App\Entity\Subscription\Subscription;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
