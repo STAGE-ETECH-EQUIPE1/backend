ba90e0e54cf9522d09e9ae8339dc43c2ac4c2ec3
Ny Fitia Ernestini <ernestininyfitia@gmail.com>
2025-07-21 15:47:02 +0300

    chore: Ad Service Google Authentication

---
+++ src/Services/Auth/GoogleAuthenticationService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services\Auth;
+
+use App\DTO\Request\GoogleAuthenticationDTO;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use League\OAuth2\Client\Token\AccessToken;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+
+class GoogleAuthenticationService
+{
+    public function __construct(
+        private ClientRegistry $clientRegistry,
+        private EntityManagerInterface $em,
+        private JWTTokenManagerInterface $jwtManager,
+    ) {}
+
+    public function authenticate(GoogleAuthenticationDTO $dto): string
+    {
+        $client = $this->clientRegistry->getClient('google');
+        $accessToken = new AccessToken(['access_token' => $dto->accessToken]);
+        $googleUser = $client->fetchUserFromToken($accessToken);
+
+        /** @var \League\OAuth2\Client\Provider\GoogleUser $googleUser */
+        $email = $googleUser->getEmail();
+
+        if (!is_string($email)) {
+            throw new \InvalidArgumentException('Email must be a string');
+        }
+
+        $user = $this->em->getRepository(User::class)->findOneBy(['email' => $email]);
+
+        if (!$user) {
+            $user = new User();
+            $user->setEmail($email);
+            $user->setFullName($googleUser->getName());
+            $user->setUsername($googleUser->getName());
+            $user->setPassword(''); // Tu peux générer un mot de passe ou laisser vide
+            $user->setPhone('');
+
+            $this->em->persist($user);
+            $this->em->flush();
+        }
+
+        return $this->jwtManager->create($user);
+    }
+}
