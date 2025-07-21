886093347f67107ff42968c20a2ffa88a6fd0102
Hasintsoa <hasinaramalanjaona@gmail.com>
2025-07-21 21:56:43 +0300

    feat: refactor authentication request to api controller path

--- src/Controller/Api/GoogleAuthenticationController.php
+++ src/Controller/Api/GoogleAuthenticationController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Api\Auth;
+namespace App\Controller\Api;
 
 use App\DTO\Request\GoogleAuthenticationDTO;
 use App\Services\Auth\GoogleAuthenticationService;
--- src/Controller/Api/ResetPasswordController.php
+++ src/Controller/Api/ResetPasswordController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Api\Auth;
+namespace App\Controller\Api;
 
 use App\DTO\Request\ResetPasswordDTO;
 use App\DTO\Request\UpdatePasswordDTO;
