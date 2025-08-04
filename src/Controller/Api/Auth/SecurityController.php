92a8d43043c2942965384510492ef798f37ccbdd
mvjdev <hei.maria.625@gmail.com>
2025-08-04 21:22:27 +0300

    fix: subscription token

--- src/Controller/Api/Auth/SecurityController.php
+++ src/Controller/Api/Auth/SecurityController.php
@@ -11,12 +11,14 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 
 class SecurityController extends AbstractController
 {
     public function __construct(
         private readonly UserServiceInterface $userService,
         private readonly EntityManagerInterface $entityManager,
+        private readonly JWTTokenManagerInterface $jwtManager,
     ) {
     }
 
@@ -47,10 +49,12 @@ class SecurityController extends AbstractController
         $user = $this->userService->convertUserRegistrationDtoToUser($userRegistrationDto);
 
         $this->entityManager->persist($user);
-        // $this->entityManager->flush();
+        $this->entityManager->flush();
+        $token = $this->jwtManager->create($user);
 
         return $this->json([
             'message' => 'User registered successfully',
+            'token' => $token,
             'user' => $this->userService->convertToDto($user),
         ], Response::HTTP_CREATED);
     }
