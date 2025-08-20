9b2ece900579a31e2ce263e613e17e91dcfae21f
mvjdev <hei.maria.625@gmail.com>
2025-08-20 17:41:28 +0300

    fix: Dockerfile

--- public/index.php
+++ public/index.php
@@ -2,10 +2,6 @@
 
 use App\Kernel;
 
-if (file_exists(dirname(__DIR__).'/.env')) {
-    (new Symfony\Component\Dotenv\Dotenv())->loadEnv(dirname(__DIR__).'/.env');
-}
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
