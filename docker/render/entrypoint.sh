0b2f63fa3700021f7da631fab26a88fb7a9beef4
mvjdev <hei.maria.625@gmail.com>
2025-08-21 17:38:22 +0300

    fix: apiplatform

--- docker/render/entrypoint.sh
+++ docker/render/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 echo "▶️ Installation des assets..."
 php bin/console assets:install --symlink --relative --env=prod || true
+php bin/console cache:clear --env=prod || true
 
 echo "▶️ Démarrage de supervisord..."
 exec "$@"
