39d8f39cffc176f1b3f20bc3d9bb02d23e57c137
mvjdev <hei.maria.625@gmail.com>
2025-08-22 14:19:19 +0300

    fix: db_url

--- docker/render/entrypoint.sh
+++ docker/render/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -e
 
-export DATABASE_URL="sqlite:///:memory:"
-
-echo "▶️ Installation des assets..."
+echo "▶Installation des assets..."
 php bin/console assets:install --symlink --relative --env=prod || true
 php bin/console cache:clear --env=prod || true
 
-echo "▶️ Démarrage de supervisord..."
+echo "▶ Démarrage de supervisord..."
 exec "$@"
