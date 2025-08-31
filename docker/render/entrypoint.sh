4140712726996ca8597bdd569b53cd29b16c4801
mvjdev <hei.maria.625@gmail.com>
2025-08-31 11:52:10 +0300

    fix env

--- docker/render/entrypoint.sh
+++ docker/render/entrypoint.sh
@@ -5,15 +5,13 @@ echo "▶Installation des assets..."
 php bin/console assets:install --symlink --relative --env=prod || true
 php bin/console cache:clear --env=prod || true
 
+# Création dossier Mercure accessible par www-data
 MERCURE_CONFIG_DIR="/var/www/html/mercure/.config"
 mkdir -p $MERCURE_CONFIG_DIR
-
-export MERCURE_PUBLISHER_JWT_KEY="a-string-secret-at-least-256-bits-long"
-export MERCURE_SUBSCRIBER_JWT_KEY="a-string-secret-at-least-256-bits-long"
-export MERCURE_ALLOWED_ORIGINS="*"
+chmod -R 755 $MERCURE_CONFIG_DIR
 
 # On s'assure que le binaire est exécutable
 chmod +x /var/www/html/mercure/mercure
 
-echo "▶ Démarrage de supervisord..."
+echo "▶Démarrage de supervisord..."
 exec "$@"
