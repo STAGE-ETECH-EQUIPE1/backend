43f353275defd9ba5e890105210bfbb9f284f804
Ny Fitia Ernestini <ernestininyfitia@gmail.com>
2025-09-01 20:56:58 +0300

    fix: refactoring

---
+++ src/Controller/Service/ReadServiceController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Service;
+
+use App\Services\ListService\ListServiceService;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class ReadServiceController
+{
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/service/show', name: 'show_service', methods: ['GET'])]
+    public function showServices(ListServiceService $listService): JsonResponse
+    {
+        $services = $listService->getAllServices();
+
+        return $this->json([
+            ''
+        ]);
+    }
+}
--- src/Controller/Subscription/SubscriptionController.php
+++ src/Controller/Subscription/SubscriptionController.php
@@ -27,8 +27,10 @@ class SubscriptionController extends AbstractController
     }
 
     #[IsGranted('ROLE_USER')]
-    #[Route('/subscription/create', name: 'create_subscription', methods: ['POST'])]
-    public function createSubscription(Request $request): JsonResponse
+    #[Route('/subscription/create', 
+        name: 'create_subscription', 
+        methods: ['POST'])]
+    public function __invoke(Request $request): JsonResponse
     {
         try {
             $subscriptionRequest = new SubscriptionRequest($request);
